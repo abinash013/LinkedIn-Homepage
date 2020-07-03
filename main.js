63d6d0f024a2e7f3508fafd31c4e997e88a1f183
abinash013 <45894844+abinash013@users.noreply.github.com>
2020-07-03 18:42:30 +0530

    done

---
+++ main.js
@@ -0,0 +1,24 @@
+/*
+document.addEventListener('DOMContentLoaded', init, false);
+
+function init(){
+const like = document.querySelector(".row6-like");
+const likesub = document.querySelector(".likesubdivwrapper");
+
+let flag = true;
+
+like.addEventListener('mouseover',()=>{
+
+likesub.style.display = "block";
+
+
+});
+like.addEventListener('mouseout',()=>{
+
+likesub.style.display = "none";
+
+
+});
+
+}
+*/
